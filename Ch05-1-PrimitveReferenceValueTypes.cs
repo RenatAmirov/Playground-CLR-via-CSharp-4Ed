73e71c74c07c091953766ee679a4641f6cd5df92
renat amirov <amirov.renat.1st@gmail.com>
2022-09-04 07:00:56 +0400

    p152 examples

--- Ch05-1-PrimitveReferenceValueTypes.cs
+++ Ch05-1-PrimitveReferenceValueTypes.cs
@@ -14,17 +14,86 @@ using Microsoft.CSharp.RuntimeBinder;
 ///////////////////////////////////////////////////////////////////////////////
 
 public static class Program {
-   public static void Main() {
-      PrimitiveDemo();
-      BoxingDemo();
-      ReferenceVsValue.Go();
-      Boxing.Go();
-      BoxingForInterfaceMethod.Go();
-      MutateViaInterface.Go();
-      DynamicDemo.Go();
-   }
+    public static void Main()
+    {
+        //Rnx_P146_P1();
+        //PrimitiveDemo();
+        //BoxingDemo();
+        //ReferenceVsValue.Go();
+        //Boxing.Go();
+        //BoxingForInterfaceMethod.Go();
+        //MutateViaInterface.Go();
+        //DynamicDemo.Go();
+     }
 
-   private static void PrimitiveDemo() {
+    #region Rnx_P152()
+         // Ссылочный тип (поскольку 'class')
+        class SomeRef { public Int32 x; }
+            // Значимый тип (поскольку 'struct')
+            struct SomeVal { public Int32 x; }
+            static void ValueTypeDemo()
+            {
+                SomeRef r1 = new SomeRef(); // Размещается в куче
+                SomeVal v1 = new SomeVal(); // Размещается в стеке
+                r1.x = 5; // Разыменовывание указателя
+                v1.x = 5; // Изменение в стеке
+                Console.WriteLine(r1.x); // Отображается "5"
+                Console.WriteLine(v1.x); // Также отображается "5"
+                                         // В левой части рис. 5.2 показан результат
+                                         // выполнения предыдущих строк
+                SomeRef r2 = r1; // Копируется только ссылка (указатель)
+                SomeVal v2 = v1; // Помещаем в стек и копируем члены
+                r1.x = 8; // Изменяются r1.x и r2.x
+                v1.x = 9; // Изменяется v1.x, но не v2.x
+                Console.WriteLine(r1.x); // Отображается "8"
+                Console.WriteLine(r2.x); // Отображается "8"
+                Console.WriteLine(v1.x); // Отображается "9"
+                Console.WriteLine(v2.x); // Отображается "5"
+                                         // В правой части рис. 5.2 показан результат
+                                         // выполнения ВСЕХ предыдущих строк
+            }
+ 
+    #endregion
+
+private static void Rnx_P146_P1()
+    {
+        //Byte b = 100;
+        //b = (Byte)(b + 200);
+        //Console.WriteLine(b);
+
+        //UInt32 invalid = unchecked((UInt32) (-1)); // OK
+        //Console.WriteLine(invalid);
+
+        //Byte b = 100; // Выдается исключение
+        //b = unchecked((Byte)(b + 200)); // OverflowException
+        //b = (Byte)checked(b + 200); // b содержит 44; нет OverflowException
+
+        //checked
+        //{ // Начало проверяемого блока
+        //    Byte b = 100;
+        //    b = (Byte)(b + 200); // Это выражение проверяется на переполнение
+        //    Console.WriteLine(b);
+        //}
+
+        //checked
+        //{ // Начало проверяемого блока
+        //    Byte b = 100;
+        //    b += 200; // Это выражение проверяется на переполнение
+        //    Console.WriteLine(b);
+        //}
+
+        checked
+        {
+            // Предположим, SomeMethod пытается поместить 400 в Byte
+            SomeMethod(400);
+            // Возникновение OverflowException в SomeMethod
+            // зависит от наличия в нем операторов проверки
+        }
+
+        //Console.WriteLine(b);
+    }
+
+    private static void PrimitiveDemo() {
       // The following 4 lines generate identical IL code
       int a = new int();
       int b = 0;
