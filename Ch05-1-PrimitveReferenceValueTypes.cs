33d3555588c2154574e071db4732fbc92a9afb31
renat amirov <amirov.renat.1st@gmail.com>
2022-09-11 17:06:20 +0400

    Chapter 5 - Primitive Types.

--- Ch05-1-PrimitveReferenceValueTypes.cs
+++ Ch05-1-PrimitveReferenceValueTypes.cs
@@ -24,34 +24,37 @@ public static class Program {
         //BoxingForInterfaceMethod.Go();
         //MutateViaInterface.Go();
         //DynamicDemo.Go();
+        ValueTypeDemo();
      }
 
     #region Rnx_P152()
-         // Ссылочный тип (поскольку 'class')
+        // Ссылочный тип (поскольку 'class')
         class SomeRef { public Int32 x; }
-            // Значимый тип (поскольку 'struct')
-            struct SomeVal { public Int32 x; }
-            static void ValueTypeDemo()
-            {
-                SomeRef r1 = new SomeRef(); // Размещается в куче
-                SomeVal v1 = new SomeVal(); // Размещается в стеке
-                r1.x = 5; // Разыменовывание указателя
-                v1.x = 5; // Изменение в стеке
-                Console.WriteLine(r1.x); // Отображается "5"
-                Console.WriteLine(v1.x); // Также отображается "5"
-                                         // В левой части рис. 5.2 показан результат
-                                         // выполнения предыдущих строк
-                SomeRef r2 = r1; // Копируется только ссылка (указатель)
-                SomeVal v2 = v1; // Помещаем в стек и копируем члены
-                r1.x = 8; // Изменяются r1.x и r2.x
-                v1.x = 9; // Изменяется v1.x, но не v2.x
-                Console.WriteLine(r1.x); // Отображается "8"
-                Console.WriteLine(r2.x); // Отображается "8"
-                Console.WriteLine(v1.x); // Отображается "9"
-                Console.WriteLine(v2.x); // Отображается "5"
-                                         // В правой части рис. 5.2 показан результат
-                                         // выполнения ВСЕХ предыдущих строк
-            }
+
+        // Значимый тип (поскольку 'struct')
+        struct SomeVal { public Int32 x; }
+
+        static void ValueTypeDemo()
+        {
+            SomeRef r1 = new SomeRef(); // Размещается в куче
+            SomeVal v1 = new SomeVal(); // Размещается в стеке
+            r1.x = 5; // Разыменовывание указателя
+            v1.x = 5; // Изменение в стеке
+            Console.WriteLine(r1.x); // Отображается "5"
+            Console.WriteLine(v1.x); // Также отображается "5"
+                                        // В левой части рис. 5.2 показан результат
+                                        // выполнения предыдущих строк
+            SomeRef r2 = r1; // Копируется только ссылка (указатель)
+            SomeVal v2 = v1; // Помещаем в стек и копируем члены
+            r1.x = 8; // Изменяются r1.x и r2.x
+            v1.x = 9; // Изменяется v1.x, но не v2.x
+            Console.WriteLine(r1.x); // Отображается "8"
+            Console.WriteLine(r2.x); // Отображается "8"
+            Console.WriteLine(v1.x); // Отображается "9"
+            Console.WriteLine(v2.x); // Отображается "5"
+                                        // В правой части рис. 5.2 показан результат
+                                        // выполнения ВСЕХ предыдущих строк
+        }
  
     #endregion
 
@@ -85,7 +88,7 @@ private static void Rnx_P146_P1()
         checked
         {
             // Предположим, SomeMethod пытается поместить 400 в Byte
-            SomeMethod(400);
+            //SomeMethod(400);
             // Возникновение OverflowException в SomeMethod
             // зависит от наличия в нем операторов проверки
         }
